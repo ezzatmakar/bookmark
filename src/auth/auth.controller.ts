3b80212391f8dcf6ae4ca75fa65af0c6099e0b0c
Ezzat Makar <ezzatmakar93@gmail.com>
2022-03-25 16:21:37 +0200

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post
+    signin(){
+        return 'sign in'
+    }
+
+    @Post
+    singnup() {}
+}
